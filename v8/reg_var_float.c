96c3ba2cf215cfe6d3df56684e2492f5f9fcf601
Ciro Santilli <ciro.santilli@gmail.com>
2018-10-20 00:00:04 +0000

    start some C inline examples, not yet automated

---
+++ v8/reg_var_float.c
@@ -0,0 +1,18 @@
+/* https://gcc.gnu.org/onlinedocs/gcc-4.4.2/gcc/Explicit-Reg-Vars.html
+ *
+ * https://stackoverflow.com/questions/6514537/how-do-i-specify-immediate-floating-point-numbers-with-inline-assembly/52906126#52906126
+ */
+
+#include <assert.h>
+#include <inttypes.h>
+
+int main(void) {
+    register double d0 __asm__ ("d0");
+    __asm__ ("fmov d0, #1.5" : : : "%d0");
+    assert(d0 == 1.5);
+    __asm__ (
+        "fmov d1, #2.5;"
+        "fadd d0, d0, d1"
+        : : : "%d0", "%d1");
+    assert(d0 == 4.0);
+}
