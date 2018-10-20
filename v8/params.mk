8fad6388f705d0ce0d71ba9e2a9af395d6685162
Ciro Santilli <ciro.santilli@gmail.com>
2018-10-20 00:00:00 +0000

    sve: explain why gcc -march=armv8-a

--- v8/params.mk
+++ v8/params.mk
@@ -1,5 +1,7 @@
 ARCH = aarch64
 DEFAULT_SYSROOT = /usr/aarch64-linux-gnu
+# armv8.5-a not in Ubuntu 16.04.
+# armv8.5-a+sve not in Ubuntu 18.04.
 MARCH = armv8-a
 MARCH_AS = armv8.5-a+sve
 PHONY_MAKES = linux
